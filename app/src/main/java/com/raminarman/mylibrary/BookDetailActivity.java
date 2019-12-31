c53ea0c8908042dae0ed50bf91910220a4ffc099
raminarmanfar <ramin.armanfar@gmail.com>
2019-12-31 23:00:39 +0100

    Dialog alert added to the book detail activity.

--- app/src/main/java/com/raminarman/mylibrary/BookDetailActivity.java
+++ app/src/main/java/com/raminarman/mylibrary/BookDetailActivity.java
@@ -1,9 +1,13 @@
 package com.raminarman.mylibrary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ public class BookDetailActivity extends AppCompatActivity {
 
     private TextView bookName, bookAuthor, numberOfPages, description;
     private ImageView bookImage;
+    private Button btnAddCurrently, btnAddWantRead, btnAddAlreadyRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,6 @@ public class BookDetailActivity extends AppCompatActivity {
         ArrayList<Book> books = Util.getAllBooks();
         for (Book b: books) {
             if (b.getId() == id) {
-                System.out.println(">>>>>>>>>>> " + id);
                 bookName.setText(b.getName());
                 bookAuthor.setText(b.getAuthor());
                 description.setText(b.getDescription());
@@ -39,6 +43,31 @@ public class BookDetailActivity extends AppCompatActivity {
 
             }
         }
+
+        btnAddCurrently.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(BookDetailActivity.this);
+                builder.setMessage("The book is already added to the currently reading list.");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setCancelable(false);
+                builder.show();
+
+            }
+        });
     }
 
     private void initWidgets() {
@@ -47,5 +76,9 @@ public class BookDetailActivity extends AppCompatActivity {
         description = findViewById(R.id.description);
         numberOfPages = findViewById(R.id.numberOfPages);
         bookImage = findViewById(R.id.bookImage);
+
+        btnAddCurrently = findViewById(R.id.btnAddCurrently);
+        btnAddWantRead = findViewById(R.id.btnAddWantRead);
+        btnAddAlreadyRead = findViewById(R.id.btnAddAlreadyRead);
     }
 }
