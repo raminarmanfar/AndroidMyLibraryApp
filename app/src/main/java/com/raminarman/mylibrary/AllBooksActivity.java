4f23a1a362d4427d7054318e4e59ca0aad07ea36
raminarmanfar <ramin.armanfar@gmail.com>
2019-12-31 14:19:15 +0100

    List recycler view implemented.

--- app/src/main/java/com/raminarman/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/raminarman/mylibrary/AllBooksActivity.java
@@ -1,6 +1,7 @@
 package com.raminarman.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,7 +15,7 @@ public class AllBooksActivity extends AppCompatActivity {
     // logt
     private static final String TAG = "AllBooksActivity";
 
-    private RecyclerView recyclerView;
+    private RecyclerView booksRecView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +23,22 @@ public class AllBooksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_books);
         Log.d(TAG, "onCreate: started");
 
-        recyclerView = findViewById(R.id.recyclerView);
+        booksRecView = findViewById(R.id.recyclerView);
 
-        ArrayList<String> books = new ArrayList<>();
-        books.add("Book 1");
-        books.add("Book 2");
-        books.add("Book 3");
-        books.add("Book 4");
-        books.add("Book 5");
+        BooksRecViewAdapter adapter = new BooksRecViewAdapter(this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, books);
-        recyclerView.setAdapter(adapter);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new LinearLayoutManager( this));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("Book about tigers", "Ramin", 120, "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?cs=srgb&dl=bengal-tiger-half-soak-body-on-water-during-daytime-145939.jpg&fm=jpg", "Ramin's book."));
+        books.add(new Book("Book about lions", "Amin", 944, "https://cosmos-images2.imgix.net/file/spina/photo/14772/GettyImages-691120979.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600", "Amin's book."));
+        books.add(new Book("Book about pets", "Roya", 250, "https://www.snopes.com/tachyon/2019/08/GettyImages-1005462320-e1566596015475.jpg?resize=865,452&quality=65", "Roya's book."));
+        books.add(new Book("Book about foxes", "Armin", 320, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfmn5twrZ8Bckj-yD6w-A6uRaDHwJ4_ngwdnKWPnsngN_k0O9l&s", "Armin's book."));
+        books.add(new Book("Book about monkeys", "Fawad", 410, "https://images.theconversation.com/files/290710/original/file-20190903-175663-lqb3z6.jpg?ixlib=rb-1.1.0&rect=48%2C0%2C5422%2C3603&q=45&auto=format&w=496&fit=clip", "Fawad's book."));
+        books.add(new Book("Book about dogs", "Ramin", 535, "https://image.freepik.com/free-photo/portrait-golden-retriever-laying-down-grass_53419-4404.jpg", "Ramin's second book."));
+
+        adapter.setBooks(books);
     }
 }
--- app/src/main/java/com/raminarman/mylibrary/BooksRecViewAdapter.java
+++ app/src/main/java/com/raminarman/mylibrary/BooksRecViewAdapter.java
@@ -1,30 +1,65 @@
 package com.raminarman.mylibrary;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
 public class BooksRecViewAdapter extends RecyclerView.Adapter<BooksRecViewAdapter.ViewHolder> {
     private static final String TAG = "BooksRecViewAdapter";
+
+    private ArrayList<Book> books = new ArrayList<>();
+    private Context context;
+
+    public BooksRecViewAdapter(Context context) {
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listitem_book_rec_view, null);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        holder.bookName.setText(books.get(position).getName());
+        holder.bookName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "tapped on " + books.get(position).getName() + " label.", Toast.LENGTH_LONG).show();
+            }
+        });
 
+        Glide.with(context)
+                .asBitmap()
+                .load(books.get(position).getImageUrl())
+                .into(holder.bookImage);
+        holder.bookImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "tapped on " + books.get(position).getName() + " image.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return books.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -38,5 +73,10 @@ public class BooksRecViewAdapter extends RecyclerView.Adapter<BooksRecViewAdapte
             bookImage = itemView.findViewById(R.id.bookImage);
             bookName = itemView.findViewById(R.id.bookName);
         }
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+        notifyDataSetChanged();
     }
 }
